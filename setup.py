0ab0d485e799a2e773ce6801618260cc221f6e72
Kyle <elvewyn@gmail.com>
2023-03-24 22:21:53 +0800

    bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="motoilet-kafka-client",
-    version="0.1.0",
+    version="0.2.0",
     description="Motoilet Kafka Client",
     author="Liang",
     packages=["motoilet_kafka_client"],
